0814ae23665f9b24f02510269e09aa5bd72a3ed6
BDataLab <businessdatalab@gmail.com>
2022-07-16 18:04:22 +0000

    ok

--- script-git.R
+++ script-git.R
@@ -1,3 +1,5 @@
+# Project First, create github repository from RStudio
+
 install.packages("usethis")
 library(usethis)
 
@@ -18,4 +20,32 @@ usethis::use_git()
 # 2: Not now
 # 3: For sure
 # Selection: 3
+
+# ✔ Adding files
+# ✔ Making a commit with message 'Initial commit'
+
+create_github_token() #takes you to the github account on the web
+
+gitcreds::gitcreds_set()
+# Enter password or token: ghp_V0wsMNoda8pLTe3QqKZY34d1nB
+# -> Adding new credentials...
+# -> Removing credetials from cache...
+# -> Done.
+
+
+usethis::use_github()
+# There are uncommitted changes and we're about to create and push to a new GitHub repo
+# Do you want to proceed anyway?
+# 
+# 1: For sure
+# 2: Nope
+# 3: No way
+# 
+# Selection: 1
+# ✔ Creating GitHub repository 'BDataLab/project'
+# ✔ Setting remote 'origin' to 'https://github.com/BDataLab/project.git'
+# ✔ Pushing 'master' branch to GitHub and setting 'origin/master' as upstream branch
+# ✔ Opening URL 'https://github.com/BDataLab/project'
+
+
 
